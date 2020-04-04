d3d0c0e71766461c258f977459a27de97c4b7e6d
Augusto Telles <40339166+toguvr@users.noreply.github.com>
2020-04-04 14:29:02 -0300

    subindo boilerplate

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const User = use('App/Models/User')
+const crypto = require('crypto')
+const Mail = use('Mail')
+const moment = require('moment')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('augusto_muralha@hotmail.com', 'Augusto | Repp')
+            .subject('Recuperação de senha')
+        }
+      )
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: 'algo deu errado, este email existe?' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment().subtract('2', 'days').isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({ error: { message: 'O token de recuperação expirou.' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: 'algo deu errado ao resetar sua senha.' } })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
---
+++ app/Controllers/Http/TaskController.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const Task = use('App/Models/Task')
+
+class TaskController {
+  async index ({ params, request, response, view }) {
+    const tasks = await Task.query().where('project_id', params.projects_id).with('user').fetch()
+
+    return tasks
+  }
+
+  async store ({ params, request, response }) {
+    const data = request.only([
+      'user_id',
+      'title',
+      'description',
+      'due_date',
+      'file_id'
+    ])
+
+    const task = await Task.create({ ...data, project_id: params.projects_id })
+
+    return task
+  }
+
+  /**
+   * Display a single task.
+   * GET tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+    const task = await Task.findOrFail(params.id)
+
+    return task
+  }
+
+  /**
+   * Update task details.
+   * PUT or PATCH tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+    const task = await Task.findOrFail(params.id)
+
+    const data = request.only([
+      'user_id',
+      'title',
+      'description',
+      'due_date',
+      'file_id'
+    ])
+
+    task.merge(data)
+
+    await task.save()
+
+    return task
+  }
+
+  /**
+   * Delete a task with id.
+   * DELETE tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+    const task = await Task.findOrFail(params.id)
+
+    await task.delete()
+  }
+}
+
+module.exports = TaskController
