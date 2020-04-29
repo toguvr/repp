dd48b05334476afc289c314ff42b0d43bd165c90
Augusto Telles <40339166+toguvr@users.noreply.github.com>
2020-04-29 11:50:03 -0300

    subindo projeto

---
+++ database/migrations/1588171648429_projects_schema.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProjectsSchema extends Schema {
+  up () {
+    this.table('projects', (table) => {
+      table.timestamp('set_date')
+    })
+  }
+
+  down () {
+    this.table('projects', (table) => {
+      // reverse alternations
+    })
+  }
+}
+
+module.exports = ProjectsSchema
